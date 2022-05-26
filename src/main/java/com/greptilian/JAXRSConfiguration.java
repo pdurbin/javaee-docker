4b329418f1584a4dddc639b753907fc70e38e40b
Philip Durbin <philipdurbin@gmail.com>
2022-05-26 15:05:25 -0400

    switch from Java EE 7 to Jakarta EE 9.1

--- src/main/java/com/greptilian/JAXRSConfiguration.java
+++ src/main/java/com/greptilian/JAXRSConfiguration.java
@@ -1,7 +1,7 @@
 package com.greptilian;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 /**
  * Configures a JAX-RS endpoint.
